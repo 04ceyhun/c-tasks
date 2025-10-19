599e031c20b203ba9b99b30ca078318275c43223
Ceyhun <139128305+04ceyhun@users.noreply.github.com>
2025-10-19 20:07:42 +0400

    Create task3.cpp

---
+++ cpp-tasks/cpp-tasks/task3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Bir eded daxil edin: ";
+    cin >> n;
+
+    cout << "Kvadrati: " << n * n << endl;
+    cout << "Kubu: " << n * n * n << endl;
+
+    return 0;
+}
