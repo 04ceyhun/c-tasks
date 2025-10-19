eb8d3285e675acaf87dbe81832e93123d2d45159
Ceyhun <139128305+04ceyhun@users.noreply.github.com>
2025-10-19 20:11:04 +0400

    Create task6.cpp

---
+++ cpp-tasks/task6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cout << "Uc eded daxil edin: ";
+    cin >> a >> b >> c;
+
+    cout << "Orta qiymet: " << (a + b + c) / 3 << endl;
+
+    return 0;
+}
