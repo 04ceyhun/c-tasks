59085897ff2f47380986f5eb14ec53fb1adb778a
Ceyhun <139128305+04ceyhun@users.noreply.github.com>
2025-10-19 20:11:44 +0400

    Create task8.cpp

---
+++ cpp-tasks/task8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double teref;
+    cout << "Kvadratin terefi: ";
+    cin >> teref;
+
+    cout << "Sahesi: " << teref * teref << endl;
+    cout << "Perimetri: " << 4 * teref << endl;
+
+    return 0;
+}
